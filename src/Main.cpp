36a99508dad095ea27979e8bcc622c4d78b8fabb
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-08-29 23:30:16 +1000

    Switched to MIDI input / output. I can see the ports on Jack but can't wire them to much yet. Tested it with Ardour and it crashed Ardour. Code for Process is probably the issue - it's not very sensible looking yet.

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,21 +2,36 @@
 #include <cstdio>
 #include <cstring>
 #include <jack/jack.h>
+#include <jack/midiport.h>
 
 
-jack_port_t *input_port;
-jack_port_t *output_port;
+jack_port_t *controllerInputPort;
+jack_port_t *deviceInputPort;
+jack_port_t *deviceOutputPort;
 jack_client_t *client;
 
 
 int process (jack_nframes_t nframes, void *arg)
 {
-    jack_default_audio_sample_t *in, *out;
-    
-    in = reinterpret_cast<jack_default_audio_sample_t*>(jack_port_get_buffer(input_port, nframes));
-    out = reinterpret_cast<jack_default_audio_sample_t*>(jack_port_get_buffer(output_port, nframes));
-    memcpy(out, in, sizeof(jack_default_audio_sample_t) * nframes);
-    
+    // Clear the output each process cycle.
+    jack_midi_clear_buffer(deviceOutputPort);
+
+    // Copy every input frame to the output.
+    jack_nframes_t inputEventCount = jack_midi_get_event_count(deviceInputPort);
+    for (jack_nframes_t i = 0; i < inputEventCount; i++)
+    {
+        jack_midi_event_t* event;
+        
+        if (int success = jack_midi_event_get(event, deviceInputPort, i))
+        {
+           // TODO: Copy the event data from the input to the output.
+            if (event->size > 0)
+            {
+                printf("Midi Event: BYTES %lu [%0X][%0X][%0X]\n", event->size, event->buffer[0], event->buffer[1], event->buffer[2]);
+            }
+        }
+    }
+
     return 0;
 }
 
@@ -27,12 +42,15 @@ void jack_shutdown(void *arg)
 }
 
 
+// NOTE: Need far more robust releasing of resources in the case of failure to init.
+
 int main (int argc, char* argv[])
 {
     jack_status_t status;
     jack_options_t options {JackNullOption};   
+    const char **ports;
 
-    printf("Jack Test\n");
+    printf("Jack Test:\n");
 
     client = jack_client_open("JackTest", options, &status);
     if (client == NULL)
@@ -45,13 +63,75 @@ int main (int argc, char* argv[])
         exit(1);
     }
 
-    printf("Jack opened client\n");
+    if (status)
+    {
+        if (JackServerStarted) 
+            fprintf(stderr, "JACK server started.\n");
+      
+        if (JackNameNotUnique)
+            fprintf(stderr, "unique name '%s' assigned.\n", jack_get_client_name(client));
+    }
 
+    // // Register the controller input MIDI port.
+    // controllerInputPort = jack_port_register(client, "ControllerInput", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+    // if (deviceInputPort == NULL)
+    // {
+    //     fprintf(stderr, "no more JACK input port available.\n");
+    //     exit(1);
+    // }
+
+    // Register a device input MIDI port.
+    deviceInputPort = jack_port_register(client, "DeviceInput", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+    if (deviceInputPort == NULL)
+    {
+        fprintf(stderr, "no more JACK input port available.\n");
+        exit(1);
+    }
+
+    // Register a device output MIDI port.
+    deviceOutputPort = jack_port_register(client, "DeviceOutput", JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
+    if (deviceOutputPort == NULL)
+    {
+        fprintf(stderr, "no more JACK output port available.\n");
+        exit(1);
+    }
+    
+    // Set the callback for the main process.
+    jack_set_process_callback(client, process, 0);
+    
+    // Set the callback for the shutdown process.
+    jack_on_shutdown(client, jack_shutdown, 0);
+
+    // Tell the server we're ready to process.
+    if (jack_activate(client))
+    {
+        fprintf(stderr, "cannot activate client");
+        exit(0);
+    }
+    
+    // Get all the ports we opened.
+    ports = jack_get_ports(client, NULL, NULL, JackPortIsPhysical | JackPortIsOutput);
+    
+    if (ports == NULL)
+    {
+        fprintf(stderr, "no physical capture ports\n");
+        exit(1);
+    }
+    
+    // if (jack_connect(client, ports[0], jack_port_name(input_port)))
+    // {
+    //     fprintf(stderr, "cannot connect input ports\n");
+    // }
+    
+    // Wait until the '.' key is pressed. Keeping the client open for a while.
     while (getchar() != '.')
     {
     }
 
+    // We need to close all the ports by hand.
+    jack_free(ports);
 
+    // We're finished using Jack. Tell it to cleanup and close down.
     jack_client_close(client);
 
     return 0;
