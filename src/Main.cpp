06d2cd72e3d8e61dd4690597b9742ca639376066
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-08-31 22:29:45 +1000

    First test where data is being written to the device. I used jack_midi_event_reserve instead of the wrapper function. It seems you need to provide your own frame numbers. Starting at zero for each cycle is working.

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,19 +9,38 @@ jack_port_t *outputPort;
 jack_client_t *client;
 jack_midi_data_t controlValue{0};
 jack_midi_data_t controlValueChange{1};
-jack_midi_data_t controlChange56[3] = {0x30, 0x38, 0x00};
 
 
-int process(jack_nframes_t nframes, void *arg)
+int process(jack_nframes_t frameTime, void *arg)
 {
 	// You must get the buffer each cycle.
-	void *inputBuffer = jack_port_get_buffer(inputPort, nframes);
+	void *inputBuffer = jack_port_get_buffer(inputPort, frameTime);
 
 	// You must get the buffer each cycle.
-	void *outputBuffer = jack_port_get_buffer(outputPort, nframes);
+	void *outputBuffer = jack_port_get_buffer(outputPort, frameTime);
 
 	// Clear the output each process cycle.
 	jack_midi_clear_buffer(outputBuffer);
+
+	jack_midi_data_t *writeBuffer1 = jack_midi_event_reserve(outputBuffer, 0, 3);
+	writeBuffer1[0] = 0xB0; // CC
+	writeBuffer1[1] = 0x32; // 50
+	writeBuffer1[2] = controlValue;
+
+	jack_midi_data_t *writeBuffer2 = jack_midi_event_reserve(outputBuffer, 1, 3);
+	writeBuffer2[0] = 0xB0; // CC
+	writeBuffer2[1] = 0x34; // 52
+	writeBuffer2[2] = controlValue;
+
+	jack_midi_data_t *writeBuffer3 = jack_midi_event_reserve(outputBuffer, 2, 3);
+	writeBuffer3[0] = 0xB0; // CC
+	writeBuffer3[1] = 0x37; // 55
+	writeBuffer3[2] = controlValue;
+
+	jack_midi_data_t *writeBuffer4 = jack_midi_event_reserve(outputBuffer, 3, 3);
+	writeBuffer4[0] = 0xB0; // CC
+	writeBuffer4[1] = 0x38; // 56
+	writeBuffer4[2] = controlValue;
 
 	// Get number of events, and process them.
 	jack_nframes_t inputEventCount = jack_midi_get_event_count(inputBuffer);
@@ -39,14 +58,6 @@ int process(jack_nframes_t nframes, void *arg)
 			controlValueChange = 1;
 		}
 
-		controlChange56[0] = 0x30;
-		controlChange56[1] = 0x38;
-		controlChange56[2] = controlValue;
-
-		// Send the data out to the device.
-		printf("Midi Write: [%0X]\n", controlValue);
-		jack_midi_event_write(outputBuffer, nframes, controlChange56, 3);
-
 		for (jack_nframes_t i = 0; i < inputEventCount; i++)
 		{
 			jack_midi_event_t *event;
@@ -54,78 +65,84 @@ int process(jack_nframes_t nframes, void *arg)
 			int success = jack_midi_event_get(event, inputBuffer, i);
 			if (success == 0)
 			{
-				if (event->size == 1)
+				// Ignore 0xF8 - seems to be a keep-alive from the Triton.
+				if (event->buffer[0] != 0xF8)
 				{
-					// Ignore 0xF8 - seems to be a keep-alive from the Triton.
-					if (event->buffer[0] != 0xF8)
+					if (event->size == 1)
 					{
 						printf("Midi Event: BYTES %lu [%0X]\n", event->size, event->buffer[0]);
 					}
-				}
-				else if (event->size == 2)
-				{
-					printf("Midi Event: BYTES %lu [%0X][%0X]\n", event->size, event->buffer[0], event->buffer[1]);
-				}
-				else if (event->size == 3)
-				{
-					// printf("Midi Event: BYTES %lu [%0X][%0X][%0X]\n", event->size, event->buffer[0],
-					// event->buffer[1], event->buffer[2]);
-				}
-				else
-				{
-					printf(
-					    "Midi Event: BYTES %lu [%0X][%0X][%0X]...\n", event->size, event->buffer[0], event->buffer[1],
-					    event->buffer[2]);
-				}
-
-				// Let's examine the first byte to determine the message type.
-				jack_midi_data_t firstByte = event->buffer[0];
-				jack_midi_data_t channel = firstByte & 0x0F;
-				jack_midi_data_t messageType = (firstByte >> 4) & 0x07;
-
-				switch (messageType)
-				{
-					case 0:
+					else if (event->size == 2)
+					{
+						printf("Midi Event: BYTES %lu [%0X][%0X]\n", event->size, event->buffer[0], event->buffer[1]);
+					}
+					else if (event->size == 3)
+					{
+						// printf("Midi Event: BYTES %lu [%0X][%0X][%0X]\n", event->size, event->buffer[0],
+						// event->buffer[1], event->buffer[2]);
+					}
+					else
+					{
 						printf(
-						    "Note Off:        Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+						    "Midi Event: BYTES %lu [%0X][%0X][%0X]...\n", event->size, event->buffer[0],
+						    event->buffer[1], event->buffer[2]);
+					}
 
-					case 1:
-						printf(
-						    "Note On:         Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+					// Let's examine the first byte to determine the message type.
+					jack_midi_data_t firstByte = event->buffer[0];
+					jack_midi_data_t channel = firstByte & 0x0F;
+					jack_midi_data_t messageType = (firstByte >> 4) & 0x07;
 
-					case 2:
-						printf(
-						    "P. Aftertouch:   Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+					switch (messageType)
+					{
+						case 0:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    Note Off:        Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
 
-					case 3:
-						printf(
-						    "Control Change:  Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+						case 1:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    Note On:         Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
 
-					case 4:
-						printf(
-						    "Program Change:  Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+						case 2:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    P. Aftertouch:   Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
 
-					case 5:
-						printf(
-						    "C. Aftertouch:   Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+						case 3:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    Control Change:  Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
 
-					case 6:
-						printf(
-						    "Pitch Wheel:     Chan: [%0X]    Data: [%0X][%0X]\n", channel, event->buffer[1],
-						    event->buffer[2]);
-						break;
+						case 4:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    Program Change:  Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
+
+						case 5:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    C. Aftertouch:   Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
+
+						case 6:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    Pitch Wheel:     Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
+
+						default:
+							printf(
+							    "Time: [%0X]    Msg: [%0X]    Unknown:         Chan: [%0X]    Data: [%0X][%0X]\n",
+							    frameTime, event->buffer[0], channel, event->buffer[1], event->buffer[2]);
+							break;
+					}
 				}
 			}
 			else
